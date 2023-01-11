e291586dc2007065ddaa63571c260bc8cee545ce
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2023-01-11 23:21:33 +0100

    Change style of inputs for title and icon, and add option to select Expense or Income

--- expense-management-app/Controllers/CategoryController.cs
+++ expense-management-app/Controllers/CategoryController.cs
@@ -45,7 +45,7 @@ namespace expense_management_app.Controllers
         // GET: Category/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new Category());
         }
 
         // POST: Category/Create
