4acc9f10113cbd04c297b072b7811af6b59b0dd9
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2023-01-13 19:36:30 +0100

    Style Edit View

--- expense-management-app/Controllers/CategoryController.cs
+++ expense-management-app/Controllers/CategoryController.cs
@@ -85,7 +85,7 @@ namespace expense_management_app.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CategoryId,Title,Icon,Type")] Category category)
+        public async Task<IActionResult> Edit(int? id, [Bind("CategoryId,Title,Icon,Type")] Category category)
         {
             if (id != category.CategoryId)
             {
