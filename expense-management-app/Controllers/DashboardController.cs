684a6cf312a4d3c0dc6fcd1ccbeb7ae724e3f6ec
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2023-01-21 19:30:38 +0100

    Add total expnese, total income and balance logic and show it in view

--- expense-management-app/Controllers/DashboardController.cs
+++ expense-management-app/Controllers/DashboardController.cs
@@ -1,11 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using expense_management_app.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace expense_management_app.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Expense")
+                .Sum(j => j.Amount);
+            ViewBag.TotalExpense = TotalExpense.ToString("C0");
+
+
+            int TotalBalance = TotalIncome - TotalExpense;
+            ViewBag.TotalBalance = TotalBalance.ToString("C0");
+
             return View();
         }
     }
