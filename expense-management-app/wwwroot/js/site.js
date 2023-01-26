7894a2d528bfe1dbd4891bc0a6c9ce22eed59fd6
Sebastian Majcher <SebastianMajcher2005@gmail.com>
2023-01-26 23:50:59 +0100

    Add buttons and content to sidebar

--- expense-management-app/wwwroot/js/site.js
+++ expense-management-app/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    dockBar = document.getElementById('sidebar').ej2_instances[0];
+    document.getElementById('sidebar-toggler').onclick = function () {
+        dockerBar.toggle();
+    };
+});
